eb31c67eaa110a53c8a6fcddf59c4fa6df856e61
veeresh <veereshmh407@gmail.com>
2025-05-11 13:34:07 +0530

    Update page content

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      
         <h1>I' m Veeresh Hiremath</h1>
-      </header>
+      
     </div>
   );
 }
