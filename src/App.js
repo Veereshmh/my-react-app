fbf8aedd8317f2eaca842f4afa96afca3136ec2b
veeresh <veereshmh407@gmail.com>
2025-05-11 20:33:38 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Counter from './components/Counter'
 
 function App() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">      
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">  
+    <div className='grid-cols-1'>    
         <h1 className="text-3xl font-bold text-center">Hello From GitHub Pages </h1>
+        </div>
         <div class="bg-gray-200 p-4"><Count /></div>
         <div class="bg-gray-300 p-4"><Counter /></div>
-        <div class="bg-gray-400 p-4">Item 3</div>
-        <div class="bg-gray-500 p-4">Item 4</div>
         <div>
           
         </div>      
