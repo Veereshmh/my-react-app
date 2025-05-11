d3e45998d9847f8fa3972f238e93f2127d4eb61b
veeresh <veereshmh407@gmail.com>
2025-05-11 19:50:39 +0530

    changes

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+
 const Count = () => {
-    const [count, setCount] = useState[0];
+    const [count, setCount] = useState(0);
 
     const Increment = () => {
         setCount(count + 1);
