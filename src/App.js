413d6a994e23d52f0be25aa8990c32e9c3d5a24e
veeresh <veereshmh407@gmail.com>
2025-05-11 18:04:00 +0530

    Update App.js content

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       
-        <h1>Hello From GitHub Pages</h1>      
+        <h1>Hello From GitHub Pages iam from localhost</h1>      
     </div>
   );
 }
