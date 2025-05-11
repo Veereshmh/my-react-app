55255b2f5838a9cb8ecad6b9c0859969a341794a
veeresh <veereshmh407@gmail.com>
2025-05-11 18:33:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Count from './components/Count';
 
 function App() {
   return (
-    <div className="App">
-      
-        <h1>Hello From GitHub Pages iam from localhost</h1>      
+    <div className="App">      
+        <h1>Hello From GitHub Pages </h1>      
+        <Count />
     </div>
   );
 }
