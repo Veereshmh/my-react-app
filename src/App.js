b7955363d88b02197d0af60483006a1cf8258f24
veeresh <veereshmh407@gmail.com>
2025-05-12 12:29:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
 import Count from './components/Count';
-import Counter from './components/Counter'
+import Counter from './components/Counter';
+import CollapsibleFAQ from './components/CollapsibleFAQ';
 
 function App() {
   return (
@@ -9,9 +10,10 @@ function App() {
     <div className='grid-cols-1 mb-20'>    
         <h1 className="text-3xl font-bold text-center mt-10">Hello From GitHub Pages </h1>
         </div>
-        <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 p-10 bg-orange-200'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 p-10 bg-orange-200'>
         <div class="bg-gray-200 p-4"><Count /></div>
         <div class="bg-gray-300 p-4"><Counter /></div> 
+        <div class="bg-gray-300 p-4"><CollapsibleFAQ /></div>
         </div>      
         
     </div>
